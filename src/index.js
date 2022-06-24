35c9c78edf93f33a4ab9c51f306593d42a813d29
Hanno Seegers <seegersjt@outlook.com>
2022-06-24 17:50:35 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import App from './App';
 
 
 
+const functions = require('firebase-functions');
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+     response.send("Hello from Firebase!");
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 	<React.StrictMode>
